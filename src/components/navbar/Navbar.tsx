1892bca33e334ee9e049e56f0893f88b0fa7dd19
zudhhyy <tubagus.zuhdi2@gmail.com>
2024-11-04 15:44:41 +0700

    init redeploy

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between px-[4%] py-2 text-white">
       <div></div>
-      <div className="flex">
+      <div className="flex w-1/3 justify-between">
         <p>Home</p>
         <p>/</p>
         <p>Project</p>
